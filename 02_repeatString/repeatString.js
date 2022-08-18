08cf6df8d03a8fa007bfeb5236c162b29a99beca
JB <jacobbunnellca@gmail.com>
2022-08-18 15:17:40 -0400

    Exercises 2: Test 1 Passed!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
 
+
+const repeatString = function repeatStringThreeTimes(str, num) {
+    return str.repeat([3]); 
+    
 };
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
