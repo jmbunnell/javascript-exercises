e37a8f95ca3511885eba443d8a66345ab819193e
JB <jacobbunnellca@gmail.com>
2022-08-19 15:38:53 -0400

    Test 1: Passed!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
 
+
+const removeFromArray = function() {
+    let numbers = [1,2,3,4];
+    let array = numbers.filter((n) => {return n != 3});
+    return array;
 };
 
 // Do not edit below this line
