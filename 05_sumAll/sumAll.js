61950f69337dfbff826174ffd7692c67144bea64
JB <jacobbunnellca@gmail.com>
2022-08-20 17:01:26 -0400

    Test 1: Passed!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(arr) {
 
-};
+    let fullArr = [1, 2, 3, 4];
+    let sum = 0;
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+
+    for (let i = arr[0]; i <= arr[1]; i++) {
+        fullArr.push(i);
+    }
+
+    sum = fullArr.reduce(reducer);
+
+    return sum;
+}
+
+/*const sumAll = function() {
+
+};*/
 
 // Do not edit below this line
 module.exports = sumAll;
