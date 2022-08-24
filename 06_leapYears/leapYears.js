3e4065b3e8dd9f98c6054cd1098bc27bd5953ed5
JB <jacobbunnellca@gmail.com>
2022-08-24 10:49:52 -0400

    First draft of leap year function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let year = '';
+    let numYear = Number(year);
+    if (numYear % 4 == 0 && numYear % 100 != 0 || numYear % 400 == 0) {
+        return 'It is a leap year!'
+    } else {
+        return 'It is not a leap year!'
+    }
 };
 
 // Do not edit below this line
